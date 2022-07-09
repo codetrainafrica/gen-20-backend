26280ea767376f76a999a110f3b3ab80d927c19d
Tony <toneyobaze@gmail.com>
2022-07-09 14:25:46 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@
 // const http = require("http");
 
 // const server = http.createServer((request, response) => {
-//   if (request.url === "/") {
-//     response.write("<h2>Home page</h2>");
+//   response.setHeader("Content-Type", "text/html");
+//   if (request.url == "/") {
+//     response.write("<h3>Home page</h3>");
 //     response.end();
-//   } else if (request.url === "/services") {
-//     response.write("<h1>Services page</h1>");
-//     response.end();
-//   } else if (request.url === "/about") {
+//   } else if (request.url == "/about") {
 //     response.write("<h1>About page</h1>");
 //     response.end();
+//   } else if (request.url == "/member") {
+//     response.write("<h1>Member page</h1>");
+//     response.end();
 //   } else {
-//     response.write("<h1>404 Not Found!</h1>");
+//     response.write("<h1>Page not found</h1>");
 //     response.end();
 //   }
 // });
@@ -21,29 +22,26 @@
 //   console.log("server running on port 3000");
 // });
 
-//With express
+//With Express
 const express = require("express");
 
 const server = express();
 
 const handleHomeRequest = (request, response) => {
-  response.send("<h1>Home page</h1>");
+  response.send("<h3>Home page</h3>");
 };
 
 const handleAboutRequest = (request, response) => {
-  response.send("<h2>About page</h2>");
+  response.send("<h3>About page</h3>");
 };
 
-const handleServicesRequest = (request, response) => {
-  response.send("<h1>Services page</h1>");
+const handleMemberRequest = (request, response) => {
+  response.send("<h3>Member page</h3>");
 };
 
-server.use("/services", handleServicesRequest);
 server.use("/about", handleAboutRequest);
+server.use("/member", handleMemberRequest);
 server.use("/", handleHomeRequest);
-server.use("*", (request, response) => {
-  response.send("<h1>404 Not Found</h1>");
-});
 
 server.listen(3000, () => {
   console.log("server running on port 3000");
