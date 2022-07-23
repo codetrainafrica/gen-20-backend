5dd208a86b16c2da02b1b7694784bf9cf9f2f220
Tony <toneyobaze@gmail.com>
2022-07-23 17:28:20 +0000

    connect to db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const {
   getUsersController,
   postUsersController,
+  updateUser,
+  deleteUser,
 } = require("./controllers/usersController");
 
 const server = express();
@@ -15,19 +18,23 @@ server.use(
 
 server.get("/users", getUsersController);
 server.post("/users", postUsersController);
+server.put("/users", updateUser);
+server.delete("/users", deleteUser);
 
-// server.get("/login", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../public/login.html"));
-// });
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://tony:ZZJz1YKhGp2eaODU@cluster0.annapyt.mongodb.net/?retryWrites=true&w=majority"
+    );
 
-// server.post("/login", (req, res) => {
-//   const { email, password } = req.body;
-//   res.send({
-//     response: "Login successful",
-//     email,
-//   });
-// });
+    console.log("DB connected successfully");
 
-server.listen(3000, () => {
-  console.log("server running on port 3000");
-});
+    server.listen(3000, () => {
+      console.log("server running on port 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectToDB();
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,18 +1,61 @@
+const userModel = require("../models/userModel");
+
 let usersArr = [
-  { name: "Kofi", email: "kofi@email.com" },
-  { name: "Ama", email: "ama@email.com" },
-  { name: "Mary", email: "mary@email.com" },
+  { name: "Kofi", email: "kofi@email.com", id: 1 },
+  { name: "Ama", email: "ama@email.com", id: 2 },
+  { name: "Mary", email: "mary@email.com", id: 3 },
 ];
 
-const getUsersController = (request, response) => {
+const getUsersController = async (request, response) => {
+  try {
+    const users = await userModel.find();
+    response.send(users);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateUser = async (request, response) => {
+  const userId = request.query.id;
+  const updatedInfo = request.body;
+
+  const updatedUser = await userModel.findByIdAndUpdate(
+    userId,
+    {
+      $set: updatedInfo,
+    },
+    { new: true }
+  );
+
+  response.send(updatedUser);
+};
+
+const deleteUser = (request, response) => {
+  const userId = request.query.id;
+
+  usersArr = usersArr.filter((user) => {
+    return user.id != userId;
+  });
+
   response.send(usersArr);
 };
 
-const postUsersController = (request, response) => {
-  const user = request.body;
-  usersArr.push(user);
+const postUsersController = async (request, response) => {
+  try {
+    const user = request.body;
+    const newUser = new userModel(user);
 
-  response.send(usersArr);
+    await newUser.save();
+
+    response.send(newUser);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = { getUsersController, postUsersController };
+module.exports = {
+  getUsersController,
+  postUsersController,
+  updateUser,
+  deleteUser,
+};
